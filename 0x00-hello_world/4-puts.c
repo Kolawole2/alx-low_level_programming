4b616f0dae94ab3a20a1b21fdeb4f0265ad52c41
kolawole <kolawoleomotesho@gmail.com>
2022-09-08 17:58:07 -0700

    ff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+#include < stdio.h >
+
+
 
 /**
  * main - Entry point
  *
- * Return Always 0 (success)
+ * Return : success always
  */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
