42836aad157c80c5f6a2867de230a39a938ec6be
kolawole <kolawoleomotesho@gmail.com>
2022-09-08 18:10:30 -0700

    ffddss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include < stdio.h >
-
-
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return : success always
- */
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
