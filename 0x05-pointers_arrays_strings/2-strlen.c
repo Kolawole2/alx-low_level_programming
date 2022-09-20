cbb35772d4bfcd7d51f7864abf32fe8abfdd06d7
kolawole <kolawoleomotesho@gmail.com>
2022-09-19 17:02:08 -0700

    fd

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,32 @@
 /**
  *_strlen - returns the length of a string
  *@s: string
- *Returns: returns length as integer;
+ *Return: returns length as integer
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while(*(s + len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
 }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
