3079e90d74d34bb2a8530b1b16f18a1fc924d4e0
kolawole <kolawoleomotesho@gmail.com>
2022-09-21 17:37:50 -0700

    fd

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 /**
  * void prototypes()
@@ -8,6 +7,7 @@
  * char prototypes()
  */
 
+int _putchar(char c);
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
