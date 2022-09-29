5be049c0137a88d34d8e85eb895725273c368df1
kolawole <kolawoleomotesho@gmail.com>
2022-09-28 17:43:08 -0700

    df

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
-	       return;
+		_putchar('\n');
+		return;
 	}
 	_putchar(*s);
-         puts_recursion(s + 1);
+	_puts_recursion(s + 1);
 }
